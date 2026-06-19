b69b0676126f115b90ed07b2c5dc96227f875a2a
Fabiano Maximiano <fabianomaximiano@hotmail.com>
2026-06-19 16:00:00 -0300

    fix(customizer): restore home panel and recover theme settings

--- wp-content/themes/mktpresenca-digital/functions.php
+++ wp-content/themes/mktpresenca-digital/functions.php
@@ -4,4 +4,5 @@
     require_once get_template_directory() . '/inc/theme-setup.php';
     require_once get_template_directory() . '/inc/enqueue.php';
     require_once get_template_directory() . '/inc/helpers.php';
-require_once get_template_directory() . '/inc/post-types.php';
+    require_once get_template_directory() . '/inc/post-types.php';
+    require_once get_template_directory() . '/inc/customizer.php';
