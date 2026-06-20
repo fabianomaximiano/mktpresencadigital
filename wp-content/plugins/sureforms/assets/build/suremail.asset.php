56f087b810bd8c0698179512de8123e5a2c3e2d7
Fabiano Maximiano <fabianomaximiano@hotmail.com>
2026-06-20 18:58:21 -0300

    chore(project): remove plugins de terceiros do versionamento

--- wp-content/plugins/sureforms/assets/build/suremail.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => '0de3d83ad850d828995b');
--- wp-content/plugins/ultimate-addons-for-gutenberg/admin-core/assets/build/dashboard-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => '167f38d8408fa341d313');
